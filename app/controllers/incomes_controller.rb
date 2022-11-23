74d3344ac37b7ee601adad406a0390a7af3ae375
tavo <gustavo.e1294@gmail.com>
2022-11-23 12:15:27 -0600

    se agrego calendario en income y se arreglo lo que estaba descuadrado en la pagina de usuarios

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -2,8 +2,10 @@ class IncomesController < ApplicationController
     before_action :authenticate_user!
     
     def index
-        @end_date = Date.today.end_of_month
+        end_date = Date.today.end_of_month
+        first_date =Date.today.at_beginning_of_month
        @total =  Payment.sum("total") 
+       @total_month =  Payment.where(:created_at => first_date..end_date).sum(:total)
     end
 
    
