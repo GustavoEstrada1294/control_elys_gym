3ee320889994087d12209c5c88c82fe7721f1bd0
tavo <gustavo.e1294@gmail.com>
2022-11-29 10:35:26 -0600

    se agredo cards a susciption types y una tabla para los nombres de los clientes

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -2,11 +2,21 @@ class IncomesController < ApplicationController
     before_action :authenticate_user!
     
     def index
+        if params[:start_date].present? && params[:end_date].present?
+            first_date = params[:start_date]
+            end_date = params[:end_date]
+            @total_range =  Payment.where(:created_at => first_date..end_date).sum(:total)
+        
+        
+            end_date = Date.today.end_of_month
+            first_date =Date.today.at_beginning_of_month    
+        end
         end_date = Date.today.end_of_month
         first_date =Date.today.at_beginning_of_month
-       @total =  Payment.sum("total") 
-       @total_month =  Payment.where(:created_at => first_date..end_date).sum(:total)
+        @total_month =  Payment.where(:created_at => first_date..end_date).sum(:total)
+        @total =  Payment.sum("total") 
+        
     end
 
-   
+    
 end
