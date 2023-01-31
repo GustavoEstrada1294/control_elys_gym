2b6cdc244660eda4757209998ec58e92b3ed2c95
tavo <gustavo.e1294@gmail.com>
2023-01-31 12:09:32 -0600

    se arreglo los pagos por rango y mitades en mixtos y  paquete

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -2,21 +2,41 @@ class IncomesController < ApplicationController
     before_action :authenticate_user!
     
     def index
-        #@user.where("user_id = ?", current_user)
-        #Payment.joins(:client).where("clients.user_id = ?", current_user )
         if params[:start_date].present? && params[:end_date].present?
             first_date = params[:start_date]
             end_date = params[:end_date]
+            
             @total_range =  Payment.joins(:client).where("clients.user_id = ?", current_user ).where(:created_at => first_date..end_date).sum(:total)
         
         
-            end_date = Date.today.end_of_month
-            first_date =Date.today.at_beginning_of_month    
+           
         end
         end_date = Date.today.end_of_month
         first_date =Date.today.at_beginning_of_month
         
-        @total_month = Payment.joins(:client).where("clients.user_id = ?", current_user ).where(:created_at => first_date..end_date).sum(:total)
+        string_1 = <<-SQL
+            SELECT 
+                SUM(CASE
+                    WHEN subscription_types.name = 'mixto' 
+                    THEN subscription_types.total / 2 
+                    WHEN subscription_types.name = 'paquete' 
+                    THEN subscription_types.total / 2 
+                    ELSE subscription_types.total
+                END) as total_m
+                FROM payments 
+                INNER JOIN clients
+                ON payments.client_id = clients.id
+                INNER JOIN subscription_types 
+                ON payments.subscription_type_id = subscription_types.id
+                WHERE clients.user_id = #{current_user.id}
+                AND payments.created_at BETWEEN '#{first_date}' AND '#{end_date}'
+        SQL
+    
+        @total_month = ActiveRecord::Base.connection.execute(string_1)
+        
+
+
+
         @total = Payment.joins(:client).where("clients.user_id = ?", current_user ).sum("total")
 
 
@@ -40,4 +60,5 @@ class IncomesController < ApplicationController
     end
     
 end
- 
+  #WHERE client.user_id = "#{current_user.id}"
+#WHERE payments.created_at = "#{created_at => first_date..end_date}"
